e125d780d51a676d184488226661f85e371d9b4c
Virginia Vieira <152438496+VirVieira@users.noreply.github.com>
2025-01-24 16:22:37 +0000

    #Atualizações
    - Criação de ContactDetailActivity
    - Intent Explícita
    - Compartilhando dados

--- app/src/main/java/com/devspace/recyclerview/ContactListAdapter.kt
+++ app/src/main/java/com/devspace/recyclerview/ContactListAdapter.kt
@@ -10,10 +10,11 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
 
-
 // Adaptação entre o data class e o item_list layout
-   class ContactListAdapter :
+class ContactListAdapter :
     ListAdapter<Contact, ContactListAdapter.ContactViewHolder>(ContactDiffutils()) {
+
+    private lateinit var onClickListener: (Contact) -> Unit
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_lis, parent, false)
@@ -22,32 +23,41 @@ import androidx.recyclerview.widget.RecyclerView
 
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
         val contact = getItem(position)
-        holder.bind(contact)
+        holder.bind(contact, onClickListener)
+    }
+
+    fun setOnClickListener(onClick: (Contact) -> Unit) {
+        onClickListener = onClick
+
     }
 
     // view holder = view que segura os dados
-    class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ContactViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         private val tvName = view.findViewById<TextView>(R.id.tv_name)
         private val tvPhone = view.findViewById<TextView>(R.id.tv_phone)
         private val image = view.findViewById<ImageView>(R.id.image)
 
-        fun bind(contact: Contact) {
+        fun bind(contact: Contact, onClick: (Contact) -> Unit) {
             tvName.text = contact.name
             tvPhone.text = contact.phone
             image.setImageResource(contact.icon)
-        }
 
-
+            view.setOnClickListener {
+                onClick.invoke(contact)
+            }
         }
 
     }
-                // compara a diferenca quando nossa lista é atualizada
-    class ContactDiffutils : DiffUtil.ItemCallback<Contact>() {
-        override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean {
-            return oldItem == newItem
-        }
 
-        override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
-            return oldItem.name == newItem.name
-        }
+}
+
+// compara a diferenca quando nossa lista é atualizada
+class ContactDiffutils : DiffUtil.ItemCallback<Contact>() {
+    override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+        return oldItem.name == newItem.name
+    }
 }
